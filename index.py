0d646e5672c146b60b37cc6537e4320377e7cea8
Group 24 <hammer.21@wright.edu>
2019-04-09 14:42:21 -0400

    Got lcd workin with ip and port

--- index.py
+++ index.py
@@ -19,26 +19,12 @@ import RPi.GPIO as GPIO
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
+import subprocess
 
 # Server Variables
 modes = {'boil': 100, 'hot':65, 'warm': 35}
 # Raspberry Pi pin setup
 tmp_sensor = 27
-# lcd_rs = 25
-# lcd_en = 24
-# lcd_d4 = 23
-# lcd_d5 = 17
-# lcd_d6 = 18
-# lcd_d7 = 22
-lcd_rs = digitalio.DigitalInOut(board.D25)
-lcd_en = digitalio.DigitalInOut(board.D24)
-lcd_d7 = digitalio.DigitalInOut(board.D22)
-lcd_d6 = digitalio.DigitalInOut(board.D18)
-lcd_d5 = digitalio.DigitalInOut(board.D17)
-lcd_d4 = digitalio.DigitalInOut(board.D23)
-# Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows = 2
 # Initialize the Flask application
 application = Flask(__name__)
 # Initialize the temperature sensor object
@@ -46,11 +32,25 @@ sensor = W1ThermSensor()
 # Setup GPIO ports
 GPIO.setmode(GPIO.BCM)
 
+lcd_rs = digitalio.DigitalInOut(board.D26)
+lcd_en = digitalio.DigitalInOut(board.D19)
+lcd_d7 = digitalio.DigitalInOut(board.D21)
+lcd_d6 = digitalio.DigitalInOut(board.D22)
+lcd_d5 = digitalio.DigitalInOut(board.D24)
+lcd_d4 = digitalio.DigitalInOut(board.D25)
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
+cmd = ['hostname', '-I']
+proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+o, e = proc.communicate()
+lcd.message = o.decode('ascii') + '\nPort: 5000'
+
 # Main page of the website
 @application.route("/")
 def index():
-    lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-    return "Hello World!"
+    return 'Hello World!'
 
 # Sets the temperature of the water heater
 @application.route('/temperature/<mode>', methods=['GET', 'POST'])
@@ -77,7 +77,7 @@ def stats():
 
 # Run the server
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', port=80)
 
 
 def flip_switch(switch):
