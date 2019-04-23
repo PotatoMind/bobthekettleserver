dc9451fe5d8c87699cc2a49b75e78dbf16b4e882
Group 24 <hammer.21@wright.edu>
2019-04-23 01:45:32 -0400

    Production server with multi-threading. Sqlite DB creating and saving. Logging added.

--- index.py
+++ index.py
@@ -12,14 +12,14 @@ from os.path import basename
 import os
 import numpy as np
 import json
-import PIL
-from PIL import Image
 from w1thermsensor import W1ThermSensor
 import RPi.GPIO as GPIO
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
 import subprocess
+from waitress import serve
+import sqlite3
 
 # Server Variables
 modes = {'boil': 90, 'hot': 60, 'warm': 30}
@@ -28,7 +28,10 @@ tmp_sensor = 27
 # Initialize the Flask application
 application = Flask(__name__)
 # Initialize the temperature sensor object
-sensor = W1ThermSensor()
+try:
+    sensor = W1ThermSensor()
+except:
+    print("DEAD")
 # Setup GPIO ports
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
@@ -47,6 +50,63 @@ proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 o, e = proc.communicate()
 lcd.message = o.decode('ascii') + '\nPort: 5000'
 
+# DB Helper Functions
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def create_db():
+    # sqlite db
+    conn = create_connection('server.db')
+    c = conn.cursor()
+
+    # Create table
+    c.execute('''CREATE TABLE IF NOT EXISTS log
+                 (datetime, command, device, response)''')
+    # Save (commit) the changes
+    conn.commit()
+    conn.close()
+    return 'Created DB'
+    
+def save_db(log):
+    # sqlite db
+    conn = create_connection('server.db')
+    c = conn.cursor()
+
+    # Create table
+    sql = '''INSERT INTO log(datetime, command, device, response)
+        VALUES(?, ?, ?, ?) '''
+    c.execute(sql, log)
+    # Save (commit) the changes
+    conn.commit()
+    conn.close()
+    return 'Added log'
+
+def get_db():
+    # sqlite db
+    conn = create_connection('server.db')
+    c = conn.cursor()
+
+    # Create table
+    sql = '''GET * FROM log'''
+    res = c.execute(sql)
+    # Save (commit) the changes
+    conn.commit()
+    conn.close()
+    return res
+# Create the database
+create_db()
+
 # Main page of the website
 @application.route("/")
 def index():
@@ -59,48 +119,44 @@ def temperature(mode):
         json = {
             "c":  round(sensor.get_temperature(), 2),
             "f": round(sensor.get_temperature(W1ThermSensor.DEGREES_F), 2),
-            "k": round(sensor.get_temperature(W1ThermSensor.DEGREES_K), 2)
+            "k": round(sensor.get_temperature(W1ThermSensor.KELVIN), 2)
         }
         return str(json)
     else:
         if mode in modes:
-                print('Setting temperature mode to ' + mode + ' at ' + str(modes[mode]))
-                temp = modes[mode]
-                if (round(sensor.get_temperature(), 2) < temp):
-                    flip_switch(1)
-                    time.sleep(5)
-                    while (round(sensor.get_temperature(), 2) < temp):
-                        print(round(sensor.get_temperature(), 2))
-                        time.sleep(5)
-                    flip_switch(0)
-                    time.sleep(5)
-                    return 'Done'
-                else:
-                    return 'Already heated'
+            return set_temperature(mode)
         else:
-                return 'Temperature mode not found: ' + mode
+            json = {
+                "c":  round(sensor.get_temperature(), 2),
+                "f": round(sensor.get_temperature(W1ThermSensor.DEGREES_F), 2),
+                "k": round(sensor.get_temperature(W1ThermSensor.KELVIN), 2)
+            }
+            save_db((datetime.datetime.now(), request.path, request.remote_addr, str(json)))
+            return str(json)
 
 # Sets the time to keep the water heater heated
-@application.route('/heat/<time>', methods=['POST'])
-def heat(time):
-    duration = int(time)
+@application.route('/heat/<mode>/<int:duration>', methods=['POST'])
+def heat(mode, duration):
+    if mode in modes:
+        set_temperature(mode)
+    else:
+        return 'Temperature mode not found: ' + mode
     start_time = time.time()
     end_time = time.time()
     flip_switch(1)
-    time.sleep(5)
     while (end_time - start_time < duration):
+        set_temperature(mode)
+        print(end_time - start_time)
         end_time = time.time()
+        time.sleep(1)
     flip_switch(0)
-    time.sleep(5)
+    save_db((datetime.datetime.now(), request.path, request.remote_addr, 'Done'))
     return 'Done'
 
 @application.route('/stats', methods=['GET'])
 def stats():
+    rows = get_db()
     return 'Some crazy stats about the kettle that we don\'t have yet.'
-
-# Run the server
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=80)
 
 @application.route('/switch/<value>', methods=['GET'])
 def switch(value):
@@ -109,6 +165,19 @@ def switch(value):
     elif value == 'off':
         flip_switch(False)
     return 'Done did the thing'
+
+def set_temperature(mode):
+    print('Setting temperature mode to ' + mode + ' at ' + str(modes[mode]))
+    temp = modes[mode]
+    if (round(sensor.get_temperature(), 2) < temp):
+        flip_switch(1)
+        while (round(sensor.get_temperature(), 2) < temp):
+            print(round(sensor.get_temperature(), 2))
+            time.sleep(1)
+        flip_switch(0)
+        return 'Done'
+    else:
+        return 'Already heated'
 
 def flip_switch(switch):
     if switch:
@@ -123,3 +192,6 @@ def flip_switch(switch):
             time.sleep(5)
         except:
             print("Some error")
+
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=5000)
--- server.sh
+++ server.sh
@@ -1,3 +1,5 @@
-export FLASK_APP=index.py
-export FLASK_DEBUG=0
+export FLASK_APP=/home/pi/Desktop/bobthekettleserver/index.py
+export FLASK_DEBUG=1
+export FLASK_ENV=development
+export FLASK_RUN_PORT=5000
 flask run --host=0.0.0.0
